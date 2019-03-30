a919e37abb1c2d64e1939db749987180aa730e36
YoshinoriN <nayo-msoft@outlook.jp>
2019-03-30 21:51:02 +0900

    impl: AkkaExampleThree

--- src/main/scala/net/yoshinorin/akkahttpexample/http/AkkaExampleThree.scala
+++ src/main/scala/net/yoshinorin/akkahttpexample/http/AkkaExampleThree.scala
@@ -1,30 +1,33 @@
 package net.yoshinorin.akkahttpexample.http
 
+import scala.concurrent.{ExecutionContext, Future}
+import scala.io.StdIn
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.server.Directives._
 import akka.stream.ActorMaterializer
+import io.circe._
+import io.circe.generic.semiauto._
+import io.circe.syntax._
+import net.yoshinorin.akkahttpexample.models.db.Users
 import net.yoshinorin.akkahttpexample.services.UsersService
 
-import scala.concurrent.{ExecutionContext, Future}
-import scala.io.StdIn
-
-object AkkaExampleTwo extends App {
+object AkkaExampleThree extends App {
 
   implicit val actorSystem: ActorSystem = ActorSystem("akkahttpexample")
   implicit val materializer: ActorMaterializer = ActorMaterializer()
   implicit val executionContext: ExecutionContext = actorSystem.dispatcher
+
+  implicit val encodeUser: Encoder[Users] = deriveEncoder[Users]
 
   val route = get {
     pathEndOrSingleSlash {
       complete(HttpEntity(ContentTypes.`application/json`, "{\"message\": \"Hello Akka HTTP!!\"}"))
     } ~ pathPrefix("users") {
       pathPrefix(".+".r) { userName =>
-        UsersService.getUser(userName) match {
-          case Some(u) => complete(HttpEntity(ContentTypes.`application/json`, s"$u"))
-          case _ => complete(HttpEntity(ContentTypes.`application/json`, "{\"message\": \"Not Found\"}"))
-        }
+        val user = UsersService.getUser(userName).asJson
+        complete(HttpEntity(ContentTypes.`application/json`, s"$user"))
       }
     }
   }
